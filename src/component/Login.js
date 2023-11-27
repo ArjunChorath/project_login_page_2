fed4b791c0477ccb5ff2d08a7240071657ca170b
arjun <arjun.chorath@trenser.com>
2023-11-27 17:40:40 +0530

    login_validation

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,8 +1,33 @@
-import { Avatar, Box, Button, Grid, Paper, TextField, withTheme } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Button,
+  Grid,
+  Paper,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
 import "./Login.css";
 
 export const Login = () => {
+    const [userName,setUserName]=useState('');
+    const [password, setPassword] = useState('');
+    const [message,setMessage]=useState('')
+
+    const handleLogin=()=>{
+        if(!userName || !password)
+        {
+            setMessage('enter your credentials')
+            
+        }
+        else{
+
+            setMessage('login successfull')
+        }
+    setUserName('')
+    setPassword('')
+
+    }
+
   const paperStyle = {
     padding: "20px",
     height: "40vh",
@@ -11,7 +36,7 @@ export const Login = () => {
     margin: "auto auto",
     backgroundColor: "#071835",
   };
-  
+
   return (
     <Box
       bgcolor="#010916"
@@ -25,49 +50,65 @@ export const Login = () => {
             <br />
             <h2>Login</h2>
           </Grid>
+
           <Grid align="center">
-            <TextField
-              variant="outlined"
-              inputProps={{
-                style: { background: "white", borderRadius: "10px" },
-              }}
-              InputLabelProps={{
-                style: { color: "white" },
-              }}
-              placeholder="username"
-              size="small"
-              fullWidth
-              required
-            ></TextField>
-            <br />
-            <br />
-            <TextField
-              variant="outlined"
-              inputProps={{
-                style: { background: "white", borderRadius: "10px" },
-              }}
-              InputLabelProps={{
-                style: { color: "white" },
-              }}
-              placeholder="Password"
-              type="password"
-              size="small"
-              fullWidth
-              required
-            ></TextField>
-            <br />
-            <br />
-            <Button
-              variant="contained"
-              margin="25px"
+            <form>
+              <TextField
+                onChange={(e) => setUserName(e.target.value)}
+                variant="outlined"
+                type="text"
+                value={userName}
+                inputProps={{
+                  style: { background: "white", borderRadius: "10px" },
+                }}
+                InputLabelProps={{
+                  style: { color: "white" },
+                }}
+                placeholder="username"
+                size="small"
+                fullWidth
+                required
+              ></TextField>
+              <br />
+              <br />
+              <TextField
+                onChange={(e) => setPassword(e.target.value)}
+                variant="outlined"
+                inputProps={{
+                  style: { background: "white", borderRadius: "10px" },
+                }}
+                InputLabelProps={{
+                  style: { color: "white" },
+                }}
+                placeholder="Password"
+                type="password"
+                value={password}
+                size="small"
+                fullWidth
+                required
+              ></TextField>
+              <br />
+              <br />
+              <Button
+                onClick={handleLogin}
+                variant="contained"
+                margin="25px"
+                style={{
+                  color: "black",
+                  background: "white",
+                  textTransform: "capitalize",
+                }}
+              >
+                Login
+              </Button>
+            </form>
+            <p
               style={{
-                color: "black",
-                background: "white",
-                textTransform: "capitalize",
+                color: message.includes("successfull") ? "green" : "red",
               }}
             >
-              Login
-            </Button>
+              {message}
+            </p>
           </Grid>
         </Paper>
       </Grid>
