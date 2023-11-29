1be0786079c9d557c6049c313149f354042544f1
arjun <arjun.chorath@trenser.com>
2023-11-29 13:06:43 +0530

    fifth_login_setup_commit

--- src/component/Login.js
+++ src/component/Login.js
@@ -6,12 +6,20 @@ import {
   TextField,
 } from "@mui/material";
 import React, { useState } from "react";
-import "./Login.css";
+import axios from "axios";
+
 
 export const Login = () => {
     const [userName,setUserName]=useState('');
     const [password, setPassword] = useState('');
     const [message,setMessage]=useState('')
+
+    const handleSubmit=async(e)=>{
+    e.preventDefault();
+
+    
+
+    }
 
     const handleLogin=()=>{
         if(!userName || !password)
@@ -29,9 +37,10 @@ export const Login = () => {
     }
 
   const paperStyle = {
-    padding: "20px",
-    height: "40vh",
-    width: "280px",
+    borderRadius:'7px',
+    padding: "35px",
+    height: "330px",
+    width: "330px",
     alignitems: "center",
     margin: "auto auto",
     backgroundColor: "#071835",
@@ -45,71 +54,93 @@ export const Login = () => {
       sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
     >
       <Grid>
-        <Paper elevation={20} style={paperStyle}>
-          <Grid align="center">
+        <Paper elevation={0} style={paperStyle}>
+          <Grid
+            align="center"
+            sx={{
+              height: "7.5rem",
+              width: "20rem",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
             <br />
-            <h2>Login</h2>
-          </Grid>
-
-          <Grid align="center">
-            <form>
-              <TextField
-                onChange={(e) => setUserName(e.target.value)}
-                variant="outlined"
-                type="text"
-                value={userName}
-                inputProps={{
-                  style: { background: "white", borderRadius: "10px" },
-                }}
-                InputLabelProps={{
-                  style: { color: "white" },
-                }}
-                placeholder="username"
-                size="small"
-                fullWidth
-                required
-              ></TextField>
-              <br />
-              <br />
-              <TextField
-                onChange={(e) => setPassword(e.target.value)}
-                variant="outlined"
-                inputProps={{
-                  style: { background: "white", borderRadius: "10px" },
-                }}
-                InputLabelProps={{
-                  style: { color: "white" },
-                }}
-                placeholder="Password"
-                type="password"
-                value={password}
-                size="small"
-                fullWidth
-                required
-              ></TextField>
-              <br />
-              <br />
-              <Button
-                onClick={handleLogin}
-                variant="contained"
-                margin="25px"
-                style={{
-                  color: "black",
-                  background: "white",
-                  textTransform: "capitalize",
-                }}
-              >
-                Login
-              </Button>
-            </form>
-            <p
+            <h2
               style={{
-                color: message.includes("successfull") ? "green" : "red",
+                color: "white",
+                fontWeight: "500",
+                fontSize: "29px",
+                lineHeight: "2.0",
+               
               }}
             >
-              {message}
-            </p>
+              Login
+            </h2>
           </Grid>
+
+           <Box >
+            <Grid align="center">
+              <form onSubmit={handleSubmit}>
+                <TextField
+                  onChange={(e) => setUserName(e.target.value)}
+                  variant="outlined"
+                  type="text"
+                  value={userName}
+                  inputProps={{
+                    style: { background: "white", borderRadius: "10px",color:"#000000" },
+                     placeholder:"username"
+                  }}
+                  InputLabelProps={{
+                    style: { color: "white" },
+                  }}
+                 
+                  size="small"
+                  fullWidth
+                  required
+                ></TextField>
+                <br />
+                <br />
+                <TextField
+                  onChange={(e) => setPassword(e.target.value)}
+                  variant="outlined"
+                  inputProps={{
+                    style: { background: "white", borderRadius: "10px" },
+                  }}
+                  InputLabelProps={{
+                    style: { color: "white" },
+                  }}
+                  placeholder="Password"
+                  type="password"
+                  value={password}
+                  size="small"
+                  fullWidth
+                  required
+                ></TextField>
+                <br />
+                <br />
+                <Button
+                  onClick={handleLogin}
+                  variant="contained"
+                  margin="25px"
+                  style={{
+                    color: "black",
+                    background: "white",
+                    textTransform: "capitalize",
+                  }}
+                >
+                  Login
+                </Button>
+              </form>
+              <p
+                style={{
+                  color: message.includes("successfull") ? "green" : "red",
+                }}
+              >
+                {message}
+              </p>
+            </Grid>
+          </Box>
         </Paper>
       </Grid>
     </Box>
